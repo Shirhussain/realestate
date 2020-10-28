61963345c346b5e6c65498f6d0831022a39406c2
حصین <sh.danishyar@gmail.com>
2020-10-28 17:18:31 +0330

    detail done

--- realstate/views.py
+++ realstate/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
-from .models import Listing
+from .models import Listing, Realtor
 
 def home(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -11,9 +11,18 @@ def home(request):
     }
     return render(request, "realstate/home.html", context)
 
-    
+
 def about(request):
-    return render(request, "realstate/about.html", {})
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #get seller of the month 
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtor
+    }
+    return render(request, "realstate/about.html", context)
 
 
 def listings(request):
@@ -30,4 +39,9 @@ def listings(request):
     
 
 def homeDetail(request, list_id):
-    return render(request, "realstate/listing.html", {})
+    list = get_object_or_404(Listing, pk=list_id)
+
+    context = {
+        'list': list
+    }
+    return render(request, "realstate/listing.html", context)
